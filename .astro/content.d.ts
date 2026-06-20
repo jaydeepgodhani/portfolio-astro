55f5f111ed912e64a96bd0a1348b76c8b3df8fc0
Jaydeep Godhani <jaydeep@Hnvk.local>
2026-06-21 00:37:02 +0530

    knowledge base working

--- .astro/content.d.ts
+++ .astro/content.d.ts
@@ -166,7 +166,31 @@ declare module 'astro:content' {
   id: string;
   body?: string;
   collection: "blog";
-  data: InferEntrySchema<"blog">;
+  data: any;
+  rendered?: RenderedContent;
+  filePath?: string;
+}>;
+"content": Record<string, {
+  id: string;
+  body?: string;
+  collection: "content";
+  data: any;
+  rendered?: RenderedContent;
+  filePath?: string;
+}>;
+"knowledge": Record<string, {
+  id: string;
+  body?: string;
+  collection: "knowledge";
+  data: InferEntrySchema<"knowledge">;
+  rendered?: RenderedContent;
+  filePath?: string;
+}>;
+"posts": Record<string, {
+  id: string;
+  body?: string;
+  collection: "posts";
+  data: InferEntrySchema<"posts">;
   rendered?: RenderedContent;
   filePath?: string;
 }>;
--- src/content.config.ts
+++ src/content.config.ts
@@ -3,19 +3,19 @@ import { glob } from "astro/loaders";
 // Import utilities from `astro:content`
 import { defineCollection, z } from "astro:content";
 // Define a `loader` and `schema` for each collection
-const blog = defineCollection({
-  loader: glob({ pattern: "**/[^_]*.md", base: "./src/blog" }),
+const knowledge = defineCollection({
+  loader: glob({ pattern: "**/[^_]*.md", base: "./src/content/knowledge" }),
   schema: z.object({
-    title: z.string(),
-    pubDate: z.date(),
-    description: z.string(),
-    author: z.string(),
-    image: z.object({
-      url: z.string(),
-      alt: z.string(),
-    }),
-    tags: z.array(z.string()),
+    title: z.string()
   }),
 });
+
+const posts = defineCollection({
+  loader: glob({ pattern: "**/[^_]*.md", base: "./src/content/posts" }),
+  schema: z.object({
+    title: z.string(),
+  }),
+});
+
 // Export a single `collections` object to register your collection(s)
-export const collections = { blog };
+export const collections = { knowledge, posts };
